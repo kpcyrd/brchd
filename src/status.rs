2197b3b9dd37ce39830dffa9dd429f1b9d5f303d
kpcyrd <git@rxv.cc>
2020-03-24 14:02:22 +0100

    Show filename as label instead of whole path

--- src/status.rs
+++ src/status.rs
@@ -6,6 +6,7 @@ use std::cmp;
 use std::collections::BTreeMap;
 use std::iter;
 use std::io::prelude::*;
+use std::path::Path;
 use std::time::{Instant, Duration};
 
 const MAX_FILENAME_LEN: usize = 20;
@@ -130,7 +131,14 @@ impl StatusWriter {
             String::new()
         };
 
-        let mut filename = p.label.as_str();
+        let path = Path::new(p.label.as_str());
+        let filename = if let Some(file_name) = path.file_name() {
+            file_name.to_string_lossy().into_owned()
+        } else {
+            p.label.clone()
+        };
+
+        let mut filename = filename.as_str();
         if filename.len() > MAX_FILENAME_LEN {
             filename = &filename[..MAX_FILENAME_LEN];
         }
